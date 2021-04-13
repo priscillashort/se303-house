aff1cdb61ca5e218ed6c504f34604941b1327f17
priscillashort <pshort2@cocc.edu>
2021-04-13 11:35:21 -0700

    add line 3 to the conditional int the line function

--- lib/house.rb
+++ lib/house.rb
@@ -2,8 +2,10 @@ class House
     def line(number)
         if number == 1
             "This is the house that Jack built.\n"
-        else
+        elsif number == 2
             "This is the malt that lay in the house that Jack built.\n"
+        else
+            "This is the rat that ate the malt that lay in the house that Jack built.\n"
         end
     end
 end
