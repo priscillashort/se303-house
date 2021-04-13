fe7b2f8a1d48adde235ffc916d52dc60f1f43a99
priscillashort <pshort2@cocc.edu>
2021-04-13 11:32:37 -0700

    Add a conditional to the line function

--- lib/house.rb
+++ lib/house.rb
@@ -1,5 +1,9 @@
 class House
-    def line(_)
-        "This is the house that Jack built.\n"
+    def line(number)
+        if number == 1
+            "This is the house that Jack built.\n"
+        else
+            "This is the malt that lay in the house that Jack built.\n"
+        end
     end
 end
