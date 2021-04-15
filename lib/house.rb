c3ade1cf61f4fb07b84f9d016d04378e2f19292d
priscillashort <pshort2@cocc.edu>
2021-04-15 11:31:31 -0700

    change conditional to a case statement for understandability

--- lib/house.rb
+++ lib/house.rb
@@ -4,27 +4,28 @@ class House
     end
 
     def line(number)
-        if number == 1
+        case number
+        when 1
             "This is the house that Jack built.\n"
-        elsif number == 2
+        when 2
             "This is the malt that lay in the house that Jack built.\n"
-        elsif number == 3
+        when 3
             "This is the rat that ate the malt that lay in the house that Jack built.\n"
-        elsif number == 4
+        when 4
             "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        elsif number == 5
+        when 5
             "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        elsif number == 6
+        when 6
             "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        elsif number == 7
+        when 7
             "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        elsif number == 8
+        when 8
             "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        elsif number == 9
+        when 9
             "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        elsif number == 10
+        when 10
             "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-        elsif number == 11
+        when 11
             "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         else
             "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
