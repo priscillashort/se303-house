fd1598bccef1d5a4f69a786b596f43eeb8e131dc
priscillashort <pshort2@cocc.edu>
2021-05-06 12:11:47 -0700

    change the when 10 branch in phrase to recursively call phrase

--- lib/house.rb
+++ lib/house.rb
@@ -37,7 +37,7 @@ class House
         when 9
             "the priest all shaven and shorn that married #{phrase(8)}"
         when 10
-            "the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
+            "the rooster that crowed in the morn that woke #{phrase(9)}"
         else
             ""
         end
