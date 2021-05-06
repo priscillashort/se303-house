6451ef5b63b1ba53c3d76f58acc4bff740e6b55f
priscillashort <pshort2@cocc.edu>
2021-05-06 12:48:34 -0700

    Add another element to pieces and use it in the when 11 branch

--- lib/house.rb
+++ lib/house.rb
@@ -18,7 +18,7 @@ class House
         when 0
             ""
         when 11
-            "the farmer sowing his corn that kept #{phrase(number-1)}"
+            "#{pieces[number-1]}#{phrase(number-1)}"
         when 12
             "the horse and the hound and the horn that belonged to #{phrase(number-1)}"
         else
@@ -37,7 +37,8 @@ class House
             'the maiden all forlorn that milked ',
             'the man all tattered and torn that kissed ',
             'the priest all shaven and shorn that married ',
-            'the rooster that crowed in the morn that woke '
+            'the rooster that crowed in the morn that woke ',
+            'the farmer sowing his corn that kept '
         ]
     end
 end
