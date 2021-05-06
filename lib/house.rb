7d099f5ad742cbd133f1e530223449da9835642a
priscillashort <pshort2@cocc.edu>
2021-05-06 11:21:49 -0700

    make phrase return a phrase and use it in when 2 branch

--- lib/house.rb
+++ lib/house.rb
@@ -12,7 +12,7 @@ class House
         when 1
             "This is the house that Jack built.\n"
         when 2
-            "This is the malt that lay in the house that Jack built.\n"
+            "This is #{phrase}the house that Jack built.\n"
         when 3
             "This is the rat that ate the malt that lay in the house that Jack built.\n"
         when 4
@@ -37,6 +37,6 @@ class House
     end
 
     def phrase
-
+        "the malt that lay in the "
     end
 end
