3cfb01d47f0c05cf3fe2693b71d6c14228e0bc17
priscillashort <pshort2@cocc.edu>
2021-05-06 12:03:45 -0700

    change the when 7 branch in phrase to recursively call phrase

--- lib/house.rb
+++ lib/house.rb
@@ -37,7 +37,7 @@ class House
         when 6
             "the cow with the crumpled horn that tossed #{phrase(5)}"
         when 7
-            "the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
+            "the maiden all forlorn that milked #{phrase(6)}"
         else
             ""
         end
