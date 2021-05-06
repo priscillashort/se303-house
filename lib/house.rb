88bac60aab3a90ed01352e9dd0512ad75ae20935
priscillashort <pshort2@cocc.edu>
2021-05-06 12:05:13 -0700

    Add a when 8 branch to phrase and use it in the line when 8 branch

--- lib/house.rb
+++ lib/house.rb
@@ -10,7 +10,7 @@ class House
     def line(number)
         case number
         when 8
-            "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "This is #{phrase(number)}the house that Jack built.\n"
         when 9
             "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 10
@@ -38,6 +38,8 @@ class House
             "the cow with the crumpled horn that tossed #{phrase(5)}"
         when 7
             "the maiden all forlorn that milked #{phrase(6)}"
+        when 8
+            "the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
         else
             ""
         end
