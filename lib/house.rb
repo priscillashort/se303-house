ebc848cdb069af79218d39ef25ecc4543f095894
priscillashort <pshort2@cocc.edu>
2021-05-06 11:32:26 -0700

    remove the when 2 branch because it is the same as the default

--- lib/house.rb
+++ lib/house.rb
@@ -9,8 +9,6 @@ class House
 
     def line(number)
         case number
-        when 2
-            "This is #{phrase(number)}the house that Jack built.\n"
         when 3
             "This is the rat that ate the malt that lay in the house that Jack built.\n"
         when 4
