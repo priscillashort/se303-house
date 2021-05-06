a0d8cfd3700d0f4a4a1899f17d5362b7526139db
priscillashort <pshort2@cocc.edu>
2021-05-06 12:00:30 -0700

    remove the when 6 branch in line because it matches the default

--- lib/house.rb
+++ lib/house.rb
@@ -9,8 +9,6 @@ class House
 
     def line(number)
         case number
-        when 6
-            "This is #{phrase(number)}the house that Jack built.\n"
         when 7
             "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 8
