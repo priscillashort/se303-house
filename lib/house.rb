6b0502b38ff8e8b41d532744838921774dfd8dd7
priscillashort <pshort2@cocc.edu>
2021-05-06 11:55:15 -0700

    add a when 5 branch to phrase and use it in the when 5 branch of line

--- lib/house.rb
+++ lib/house.rb
@@ -10,7 +10,7 @@ class House
     def line(number)
         case number
         when 5
-            "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "This is #{phrase(number)}the house that Jack built.\n"
         when 6
             "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 7
@@ -38,6 +38,8 @@ class House
             "the rat that ate #{phrase(2)}"
         when 4
             "the cat that killed #{phrase(3)}"
+        when 5
+            "the dog that worried the cat that killed the rat that ate the malt that lay in "
         else
             ""
         end
