53a6531aafd7541d92ca4d1e054231c48a3c0f77
priscillashort <pshort2@cocc.edu>
2021-05-06 12:06:15 -0700

    change the when 8 branch in phrase to recursively call phrase

--- lib/house.rb
+++ lib/house.rb
@@ -37,7 +37,7 @@ class House
         when 7
             "the maiden all forlorn that milked #{phrase(6)}"
         when 8
-            "the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
+            "the man all tattered and torn that kissed #{phrase(7)}"
         else
             ""
         end
