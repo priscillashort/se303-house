2ef83eeb5ba161f66ba42ed680773a7f0403817e
priscillashort <pshort2@cocc.edu>
2021-05-06 12:18:57 -0700

    change the when 12 branch in phrase to recursively call phrase

--- lib/house.rb
+++ lib/house.rb
@@ -34,7 +34,7 @@ class House
         when 11
             "the farmer sowing his corn that kept #{phrase(10)}"
         when 12
-            "the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
+            "the horse and the hound and the horn that belonged to #{phrase(11)}"
         else
             ""
         end
